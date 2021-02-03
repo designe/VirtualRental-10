1adbf68ec8ec251e3aaa60871bfc63d82f5ea4b3
designe <hello.designe@facebook.com>
2021-02-03 17:04:34 +0900

    Uncommented GraphicUI and Running Well version

--- src/main/java/video/rental/demo/Main.java
+++ src/main/java/video/rental/demo/Main.java
@@ -4,13 +4,17 @@ import video.rental.demo.application.Interactor;
 import video.rental.demo.domain.Repository;
 import video.rental.demo.infrastructure.RepositoryMemImpl;
 import video.rental.demo.presentation.CmdUI;
+import video.rental.demo.presentation.GraphicUI;
 
 public class Main {
-	private static CmdUI ui;
-
+	//private static CmdUI ui;
+	private static GraphicUI mGraphicUI;
 	public static void main(String[] args) {
 		Repository repository = new RepositoryMemImpl();
-		ui = new CmdUI(new Interactor(repository));
-		ui.start();
+		//ui = new CmdUI(new Interactor(repository));
+		//ui.start();
+		mGraphicUI = new GraphicUI(new Interactor(repository));
+		mGraphicUI.start();
+		
 	}
 }
